ee7bf0c33a5eea29d5b97742b8da1a8edbfb1eeb
Zachary <zachary@Zipcoders-MacBook-Pro-12.local>
2023-03-11 16:36:03 -0500

    added 3 boolean methods for diagonal/horizontal/vertical wins

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,7 +4,11 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    public static Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
@@ -23,4 +27,20 @@ public class Board {
         return null;
     }
 
+    public boolean diagonalWin(char input){
+    return  ((input == matrix[0][0] && input == matrix[1][1] && input == matrix[2][2])
+            || (input == matrix[2][0] && input == matrix[1][1] && input == matrix[0][2]));
+    }
+
+    public boolean horizontalWin(char input){
+        return  ((input == matrix[0][0] && input == matrix[0][1] && input == matrix[0][2])
+                || (input == matrix[1][0] && input == matrix[1][1] && input == matrix[1][2])
+                || (input == matrix[2][0] && input == matrix[2][1] && input == matrix[2][2]));
+    }
+
+    public boolean verticalWin(char input){
+        return  ((input == matrix[0][0] && input == matrix[1][0] && input == matrix[2][0])
+                || (input == matrix[0][1] && input == matrix[1][1] && input == matrix[2][1])
+                || (input == matrix[0][2] && input == matrix[1][2] && input == matrix[2][2]));
+    }
 }
